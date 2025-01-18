9c5dc54b9644d651913d6eddac54cba60c0964fb
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2025-01-18 22:28:31 +0100

    commandes de haxoorr

---
+++ src/self/commands/fun/id2token.ts
@@ -0,0 +1,23 @@
+import { axios } from "ihorizon-tools";
+import type { SelfCommandType } from "../../../../types/self_commands.d.ts";
+
+export const command: SelfCommandType = {
+  name: "id2token",
+  description: "Get the first part of the token from an ID",
+  category: "fun",
+  callback: async (client, message, args) => {
+    let id = args[0];
+
+    if (!id || Number.isNaN(Number(id))) {
+      return client.send(message, {
+        content: `Command malformed 😵\n${client.prefix}${command.name} **[id]**`,
+      });
+    }
+
+    let token = Buffer.from(id).toString("base64");
+
+    client.send(message, {
+      content: `> ⚡ **Token for ID ${id}**\n${token}`,
+    });
+  },
+};
---
+++ src/self/commands/fun/ipinfo.ts
@@ -0,0 +1,39 @@
+import { axios } from "ihorizon-tools";
+import type { SelfCommandType } from "../../../../types/self_commands.d.ts";
+
+export const command: SelfCommandType = {
+  name: "ipinfo",
+  description: "Get information about an IP",
+  category: "fun",
+  callback: async (client, message, args) => {
+    let ip = args[0];
+
+    if (!ip) {
+      return client.send(message, {
+        content: `Command malformed 😵\n${client.prefix}${command.name} **[ip]**`,
+      });
+    }
+
+    let data = (await axios.get(`http://ip-api.com/json/${ip}`))?.data;
+
+    if (data.status === "fail") {
+      return client.send(message, {
+        content: `❌ **Invalid IP**`,
+      });
+    }
+
+    let formatedMessage = `> ⚡ **IP information for ${data.query}**\n
+> **Country**: ${data.country}\n
+> **Region**: ${data.regionName}\n
+> **City**: ${data.city}\n
+> **ZIP**: ${data.zip}\n
+> **ISP**: ${data.isp}\n
+
+> **Organization**: ${data.org}\n
+> **AS**: ${data.as}\n
+> **Timezone**: ${data.timezone}\n
+> **Lat/Lon**: ${data.lat}, ${data.lon}
+`;
+    client.send(message, formatedMessage);
+  },
+};
