dcdfb4663911a4c7e9b81dc1c2bd30259a7b0346
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2025-01-18 22:35:05 +0100

    Create google.ts

---
+++ src/self/commands/fun/google.ts
@@ -0,0 +1,13 @@
+import type { SelfCommandType } from "../../../../types/self_commands.d.ts";
+export const command: SelfCommandType = {
+  name: "go",
+  description: "Make a Google search",
+  category: "fun",
+  callback: async (client, message, args) => {
+    let query = args.join(" ");
+
+    client.send(message, {
+      content: `https://www.google.com/search?q=${encodeURIComponent(query)}`,
+    });
+  },
+};
