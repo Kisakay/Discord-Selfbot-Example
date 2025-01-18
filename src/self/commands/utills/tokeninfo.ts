6efee7cfa79a8c86efb9ee642ff49b1f71fbb2d9
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2025-01-18 11:56:54 +0100

    Create tokeninfo.ts

---
+++ src/self/commands/utills/tokeninfo.ts
@@ -0,0 +1,35 @@
+import type { Message } from "discord.js-selfbot-v13";
+import type { Self } from "../../self";
+import type { SelfCommandType } from "../../../../types/self_commands.d.ts";
+
+export const command: SelfCommandType = {
+    name: 'tokeninfo',
+    description: 'get token info',
+    category: "utils",
+    callback: async (client: Self, message: Message, args: string[]) => {
+        var token = args[0];
+        if (!token) return message.edit(`\`\`\`ini
+        [ Token Information - KisakaySelf ]
+       
+        Invalid token\`\`\``)
+        var tokeninfo = await fetch('https://discord.com/api/v8/users/@me', {
+            method: 'GET',
+            headers: {
+                'Authorization': token,
+            }
+        }).then(res => res.json())
+        message.edit(`\`\`\`ini
+[ Token Information - HephaistosSB ]
+
+[ User Information ]
+- Username: ${tokeninfo.username}#${tokeninfo.discriminator}
+- ID: ${tokeninfo.id}
+- Email: ${tokeninfo.email}
+- Phone: ${tokeninfo.phone}
+- Verified: ${tokeninfo.verified}
+- MFA: ${tokeninfo.mfa_enabled}
+- Flags: ${tokeninfo.flags}
+- Locale: ${tokeninfo.locale}
+- NSFW: ${tokeninfo.nsfw_allowed}\`\`\``)
+    }
+}
