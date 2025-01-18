0440e5e4d3b38e9024addf194901e90af37eb7ab
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2025-01-18 22:55:48 +0100

    nswf category

---
+++ src/self/commands/nsfw/ass.ts
@@ -0,0 +1,41 @@
+import { axios } from "ihorizon-tools";
+import type { SelfCommandType } from "../../../../types/self_commands.d.ts";
+import { MessageAttachment } from "discord.js-selfbot-v13";
+
+export const command: SelfCommandType = {
+  name: "ass",
+  description: "Show a ass image",
+  category: "nsfw",
+  callback: async (client, message, args) => {
+    try {
+      const apiResponse = await axios.get(
+        "https://nekobot.xyz/api/image?type=ass",
+      );
+      if (!apiResponse.data || !apiResponse.data.message) {
+        return client.send(
+          message,
+          "Failed to retrieve an image. Please try again later.",
+        );
+      }
+
+      const imageResponse = await axios.get(apiResponse.data.message, {
+        responseType: "arrayBuffer",
+      });
+
+      const attachment = new MessageAttachment(
+        Buffer.from(imageResponse.data),
+        "ass.png",
+      );
+
+      client.send(message, {
+        content: null,
+        files: [attachment],
+      });
+    } catch (error) {
+      return client.send(
+        message,
+        "An error occurred while fetching the image. Please try again later.",
+      );
+    }
+  },
+};
