719236a2b3c004c8693119d019749c3c0176e09e
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2025-01-19 13:34:26 +0100

    .

---
+++ src/self/commands/utills/whopingme.ts
@@ -0,0 +1,20 @@
+import type { SelfCommandType } from "../../../../types/self_commands.d.ts";
+import { time } from "discord.js";
+
+export const command: SelfCommandType = {
+  name: "whopingme",
+  description: "Get the member who pinged you",
+  category: "utils",
+  callback: (client, message, args) => {
+    let snipeData = client.db.get(`GHOST_PING.${message.channelId}`);
+
+    if (!snipeData) {
+      client.send(message, { content: "Nothing to snipe here!" });
+      return;
+    }
+
+    client.send(message, {
+      content: `⭐ __**Who Pinged Me**__ \n**Author**: <@${snipeData.author}>\n**When**:${time(new Date(snipeData.timestamp), "R")}\n**Content**: ${(snipeData.content as string).substring(0, 1900)}`,
+    });
+  },
+};
--- src/self/events/snipe_module.ts
+++ src/self/events/snipe_module.ts
@@ -3,15 +3,24 @@ import type { Self } from "../self";
 import type { SelfEventType } from "../../../types/self_event.d.ts";
 
 export const event: SelfEventType = {
-    name: 'messageDelete',
-    once: false,
-    callback: (client: Self, oldMessage: Message) => {
-        if (!oldMessage.author) return;
+  name: "messageDelete",
+  once: false,
+  callback: (client: Self, oldMessage: Message) => {
+    if (!oldMessage.author) return;
+    if (oldMessage.author.id === client.user?.id) return;
 
-        client.db.set(`SNIPE.${oldMessage.channelId}`, {
-            author: oldMessage.author.id,
-            content: oldMessage.content,
-            timestamp: Date.now(),
-        })
+    client.db.set(`SNIPE.${oldMessage.channelId}`, {
+      author: oldMessage.author.id,
+      content: oldMessage.content,
+      timestamp: Date.now(),
+    });
+
+    if (oldMessage.mentions.users.get(client.user?.id!)) {
+      client.db.set(`GHOST_PING.${oldMessage.channelId}`, {
+        author: oldMessage.author.id,
+        content: oldMessage.content,
+        timestamp: Date.now(),
+      });
     }
-}
+  },
+};
