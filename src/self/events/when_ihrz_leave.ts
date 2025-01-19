87eef381fc1e4567f30c5af0a19d35bfd1be2b6c
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2025-01-19 13:08:43 +0100

    Create when_ihrz_leave.ts

---
+++ src/self/events/when_ihrz_leave.ts
@@ -0,0 +1,20 @@
+import type { Guild, GuildMember } from "discord.js-selfbot-v13";
+import type { Self } from "../self";
+import type { SelfEventType } from "../../../types/self_event.d.ts";
+
+export const event: SelfEventType = {
+  name: "guildMemberRemove",
+  once: false,
+  callback: (client: Self, member: GuildMember) => {
+    if (client.user!.id === client.config?.ihorizon_owner_id) {
+      let state = client.db.get("notihrz");
+
+      let iHorizon_was_in_server =
+        member.user.id === client.config.ihorizon_bot_id;
+
+      if (iHorizon_was_in_server && state) {
+        member.guild.leave();
+      }
+    }
+  },
+};
