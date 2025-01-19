a260da049eefda30f2c29c86bc2477e25807b9ba
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2025-01-19 14:59:11 +0100

    Update anti_group.ts

--- src/self/events/anti_group.ts
+++ src/self/events/anti_group.ts
@@ -1,17 +1,20 @@
-import type { Channel, Message } from "discord.js-selfbot-v13";
+import type { Channel, GroupDMChannel, Message } from "discord.js-selfbot-v13";
 import type { Self } from "../self";
 import type { SelfEventType } from "../../../types/self_event.d.ts";
 
 export const event: SelfEventType = {
-    name: 'channelCreate',
-    once: false,
-    callback: (client: Self, channel: Channel) => {
-        let state = client.db.get("antigroup");
+  name: "channelCreate",
+  once: false,
+  callback: (client: Self, channel: Channel) => {
+    let state = client.db.get("antigroup");
 
-        if (channel.type === "GROUP_DM" && state) {
-            channel.delete().catch(() => false);
-        }
-
-        client.logger.warn(`Group ${channel.id} leaved.`)
+    if (
+      channel.type === "GROUP_DM" &&
+      state &&
+      (channel as GroupDMChannel)?.ownerId !== client.user!.id
+    ) {
+      client.logger.warn(`Group ${channel.id} leaved.`);
+      channel.delete().catch(() => false);
     }
-}
+  },
+};
