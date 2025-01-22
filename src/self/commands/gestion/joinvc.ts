fcc6ee8849ec6cc5baaecd5a46f7293aa391cc17
Kisakay <53227802+Kisakay@users.noreply.github.com>
2025-01-22 12:01:33 +0100

    Update joinvc.ts

--- src/self/commands/gestion/joinvc.ts
+++ src/self/commands/gestion/joinvc.ts
@@ -1,3 +1,4 @@
+import { Channel } from "discord.js-selfbot-v13";
 import type { SelfCommandType } from "../../../../types/self_commands.d.ts";
 
 export const command: SelfCommandType = {
@@ -6,9 +7,22 @@ export const command: SelfCommandType = {
   category: "gestion",
   callback: async (client, message, args) => {
     let channel =
-      message.mentions.channels.first() || client?.channels.cache.get(args[0]);
+      message.mentions.channels.first() ||
+      client?.channels.cache.get(args[0]) ||
+      args[0];
 
-    if (!channel) {
+    if (channel === "delete") {
+      client.db.delete("joinvc");
+      client.send(message, {
+        content: `✅ Voice channel deleted`,
+      });
+
+      // disconnect from voice channel
+      client.voice.connection?.disconnect();
+      return;
+    }
+
+    if (!channel || !(channel instanceof Channel)) {
       client.send(message, "😵 The channel doesn't exist");
       return;
     }
@@ -24,12 +38,12 @@ export const command: SelfCommandType = {
       content: `✅ Voice channel set to **${channel.name}**`,
     });
 
-    let vc = (await client.voice.joinChannel(channel.id, {
+    let vc = await client.voice.joinChannel(channel.id, {
       selfDeaf: true,
       selfMute: true,
       selfVideo: true,
       videoCodec: "H264",
-    }));
+    });
     vc.createStreamConnection();
   },
 };
