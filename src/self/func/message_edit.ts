684acbfb49c23e0205bafbb98dc9390e067f4390
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2025-02-21 22:53:45 +0100

    Update message_edit.ts

--- src/self/func/message_edit.ts
+++ src/self/func/message_edit.ts
@@ -3,20 +3,30 @@ import type { Message, MessageEditOptions, ReplyMessageOptions } from "discord.j
 
 let max = 15_000;
 
-export function messageEdit(message: Message, body: string | MessageEditOptions) {
-    message.edit(body)
-        .then(async x => {
-            await sleep(max);
-            if (x.deletable) x.delete();
-        })
-        .catch(() => false)
+export async function messageEdit(message: Message, body: string | MessageEditOptions, timeout?: number): Promise<Message | undefined> {
+    try {
+        const editedMessage = await message.edit(body);
+
+        setTimeout(() => {
+            if (editedMessage.deletable) editedMessage.delete().catch(() => { });
+        }, timeout || max);
+
+        return editedMessage;
+    } catch (error) {
+        return undefined;
+    }
 }
 
-export function messageSend(message: Message, body: string | ReplyMessageOptions) {
-    message.reply(body)
-        .then(async x => {
-            await sleep(max);
-            if (x.deletable) x.delete();
-        })
-        .catch(() => false)
+export async function messageSend(message: Message, body: string | ReplyMessageOptions, timeout?: number): Promise<Message | undefined> {
+    try {
+        const sentMessage = await message.reply(body);
+
+        setTimeout(() => {
+            if (sentMessage.deletable) sentMessage.delete().catch(() => { });
+        }, timeout || max);
+
+        return sentMessage;
+    } catch (error) {
+        return undefined;
+    }
 }
