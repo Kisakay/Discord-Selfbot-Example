7bad65c13c850111d4e174cdb4f61ccdc8bfab1d
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2025-02-23 15:18:11 +0100

    Update lockvanity.ts

--- src/self/commands/utils/lockvanity.ts
+++ src/self/commands/utils/lockvanity.ts
@@ -4,24 +4,21 @@ export const command: SelfCommandType = {
     name: 'lockvanity',
     description: 'Lock a vanity URL',
     category: "utils",
+    aliases: ["lockvanityurl", "vanitylock"],
     async callback(client, message, args) {
-        let option = args[0];
+        let option = await client.db.get(`lockvanity.${message.guildId}`);
 
-        if (option === "unlock") {
+        if (option) {
             await client.db.delete(`lockvanity.${message.guildId}`);
 
             client.send(message, "Vanity URL has been unlocked");
-        } else if (option === "lock") {
+        } else {
             if (!message.guild?.vanityURLCode) {
                 return client.send(message, "This server does not have a vanity URL.");
             }
 
             client.send(message, `The vanity URL \`${message.guild?.vanityURLCode}\` has been locked. When a user tries to change the vanity URL to a another one, they will be banned. And Vanity URL \`${message.guild?.vanityURLCode}\` is locked.`);
             await client.db.set(`lockvanity.${message.guildId}`, message.guild?.vanityURLCode);
-        } else {
-            client.send(message, {
-                content: `Command malformed 😵\n${await client.prefix()}${command.name} **[unlock/lock]**`,
-            });
         }
     }
 }
